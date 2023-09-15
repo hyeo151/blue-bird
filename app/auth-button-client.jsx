8aad92db6a9985db8df33345023595ce9d68bf16
khyeo151@gmail.com <khyeo151@gmail.com>
2023-09-16 11:43:44 +1200

    deploy app

---
+++ app/auth-button-client.jsx
@@ -0,0 +1,28 @@
+"use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
+export default function AuthButtonClient({ session }) {
+  const router = useRouter();
+  const supabase = createClientComponentClient();
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: { redirectTo: `${location.origin}/auth/callback` },
+    });
+  };
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  return session ? (
+    <button className="text-xs text-gray-400" onClick={handleSignOut}>
+      Logout
+    </button>
+  ) : (
+    <button className="text-xs text-gray-400" onClick={handleSignIn}>
+      Login
+    </button>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,43 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import AuthButtonServer from "./auth-button-server";
+import { redirect } from "next/navigation";
+import NewTweet from "./new-tweet";
+import Likes from "./likes";
+import Tweets from "./tweets";
+
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  const { data } = await supabase
+    .from("tweets")
+    .select("*,author: profiles(*), likes(user_id)")
+    .order("created_at", { ascending: false });
+  const tweets =
+    data?.map((tweet) => ({
+      ...tweet,
+      user_has_liked_tweet: tweet.likes.find(
+        (like) => like.user_id === session.user.id
+      ),
+      likes: tweet.likes.length,
+    })) ?? [];
+  return (
+    <div className="w-full max-w-xl mx-auto">
+      <div className="flex justify-between px-4 py-6 border border-gray-800 border-t-0">
+        <h1 className="text-xl font-bold">Home</h1>
+        <AuthButtonServer />
+      </div>
+      <NewTweet user={session.user} />
+      <Tweets tweets={tweets} />
+    </div>
+  );
+}
